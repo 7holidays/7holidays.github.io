df7182fe722ff335109ab431c834f2a8ed448972
Yuren Ju <yurenju@gmail.com>
2016-10-27 22:14:10 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <main-section></main-section>
+  </div>
+</template>
+
+<script>
+import MainSection from './components/MainSection';
+
+export default {
+  name: 'app',
+  components: {
+    MainSection,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
