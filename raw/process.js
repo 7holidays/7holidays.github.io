099e127d7eebf002bcdd2deae74750f1cc09e475
Yuren Ju <yurenju@gmail.com>
2016-10-28 21:04:39 +0800

    directly generated file from main.csv

--- raw/process.js
+++ raw/process.js
@@ -1,25 +1,29 @@
 require('shelljs/global');
 
+const Converter = require('csvtojson').Converter;
+const path = require('path');
 
-const data = JSON.parse(cat('data.json'));
+const csvFile = path.join(__dirname, 'main.csv');
+const legislatorsFile = path.join(__dirname, '..', 'src', 'legislators.json');
+const converter = new Converter({});
+converter.fromFile(csvFile, (err, data) => {
+  if (err) throw err;
 
-var obj = {};
-data.forEach(item => {
-  if (item.areaName === '全國不分區' || item.areaName.indexOf('原住民') !== -1) return;
-  
-  const city = item.areaName.substr(0, 3);
-  if (!obj[city]) {
-    obj[city] = {};
-  }
-  
-  item.district.split('、').forEach(district => {
-    if (!obj[city][district]) {
-      obj[city][district] = item;
+  const obj = {};
+  data.forEach((item) => {
+    if (item.areaName === '全國不分區' || item.areaName.indexOf('原住民') !== -1) return;
+
+    const city = item.areaName.substr(0, 3);
+    if (!obj[city]) {
+      obj[city] = {};
     }
+
+    item.district.split('、').forEach((district) => {
+      if (!obj[city][district]) {
+        obj[city][district] = item;
+      }
+    });
   });
-  
+
+  JSON.stringify(obj, null, 2).to(legislatorsFile);
 });
-
-// console.log(obj);
-
-JSON.stringify(obj, null, 2).to('../src/legislators.json');
